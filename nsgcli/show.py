39c8773aa28fbb75bbf17be69a199f9956ecfcbc
Vadim Kurland <vadim@happygears.net>
2018-08-07 20:56:40 -0700

    command 'show index'

--- nsgcli/show.py
+++ nsgcli/show.py
@@ -9,8 +9,11 @@ This module implements subset of NetSpyGlass CLI commands
 from __future__ import print_function
 
 import datetime
+import dateutil.relativedelta
 import json
 import time
+
+from typing import Dict, Any
 
 import nsgcli.api
 import nsgcli.sub_command
@@ -99,44 +102,6 @@ class ShowCommands(nsgcli.sub_command.SubCommand, object):
             print(json.dumps(resp_json, indent=4))
 
     ##########################################################################################
-    def do_index(self, arg):
-        """
-        List NsgQL indexes and their cardinality
-        """
-        status, resp_json = self.get_cache_data()
-        if status != 200:
-            print('ERROR: {0}'.format(self.get_error(resp_json)))
-        else:
-            indexes = resp_json[0].get('INDEXES', {})
-            format = '{0:<40} | {1:<20} | {2:<10} | {3:<5} | {4}'
-            print(format.format('index', 'table', 'column', 'ext', 'cardinality'))
-            print('----------------------------------------------------------------------------------------------------')
-            for index_key in sorted(indexes.keys()):
-                table, column, suffix = self.parse_index_key(index_key)
-                print(format.format(index_key, table, column, suffix, indexes[index_key]))
-
-    def parse_index_key(self, index_key):
-        """
-        parse index key and return table, column and suffix
-
-        currently recognized format is
-
-        table:column[.suffix]
-        """
-        first_colon = index_key.find(':')
-        if first_colon < 0:
-            return '', '', ''
-        table = index_key[0 : first_colon]
-        last_dot = index_key.rfind('.')
-        if last_dot < 0:
-            column = index_key[first_colon + 1:]
-            suffix = ''
-        else:
-            column = index_key[first_colon + 1: last_dot]
-            suffix = index_key[last_dot + 1:]
-        return table, column, suffix
-
-    ##########################################################################################
     def do_server(self, arg):
         """
         Print server status
@@ -223,7 +188,8 @@ class ShowCommands(nsgcli.sub_command.SubCommand, object):
                     response = json.loads(response.content)
                     filtered = {}
                     for k in response.keys():
-                        if k in ['links', 'nodes', 'path', 'singleUser', 'defaultVar', 'rule', 'linkRule', 'generation']:
+                        if k in ['links', 'nodes', 'path', 'singleUser', 'defaultVar', 'rule', 'linkRule',
+                                 'generation']:
                             continue
                         filtered[k] = response[k]
                     print(json.dumps(filtered, indent=4))
@@ -256,3 +222,88 @@ class ShowCommands(nsgcli.sub_command.SubCommand, object):
                 suffix = ' outdated'
             return value.strftime('%Y-%m-%d %H:%M:%S') + suffix
         return value
+
+    ##########################################################################################
+    def do_index(self, arg):
+        """
+        List NsgQL indexes and their cardinality
+        """
+        request = 'v2/ui/net/{0}/actions/indexes/list'.format(self.netid)
+        try:
+            response = nsgcli.api.call(self.base_url, 'GET', request, token=self.token)
+        except Exception as ex:
+            return 503, ex
+        else:
+            ordered_columns = ['table', 'column', 'suffix', 'type', 'cardinality', 'redisKey', 'updatedAt']
+            title = {'table': 'table',
+                     'column': 'column',
+                     'suffix': 'ext',
+                     'redisKey': 'redis',
+                     'type': 'type',
+                     'cardinality': 'cardinality',
+                     'updatedAt': 'updatedAt'
+                     }
+            resp = json.loads(response.content)
+            column_width = {}  # type: Dict[Any, Any]
+            for idx in resp:
+                converted = self.convert_obj(idx, ordered_columns)
+                for column_name in ordered_columns:
+                    self.update_column_width(converted, column_name, column_width)
+                    self.update_column_width(title, column_name, column_width)
+            # assemble format string
+            table_columns = []
+            for column_name in ordered_columns:
+                table_columns.append('{{0[{0}]:<{1}}}'.format(column_name, column_width.get(column_name)))
+            format_str = ' | '.join(table_columns)
+            title_line = format_str.format(title)
+            print(title_line)
+            print('-' * len(title_line))
+            for idx in resp:
+                converted = self.convert_obj(idx, ordered_columns)
+                print(format_str.format(converted))
+
+    def convert_obj(self, obj, columns):
+        new_obj = obj.copy()
+        # fill in required columns that may be missing in the object
+        for col in columns:
+            if col not in new_obj:
+                new_obj[col] = ''
+        updated_at = int(obj['updatedAt'])
+        if updated_at == 0:
+            new_obj['updatedAt'] = '--'
+        else:
+            new_obj['updatedAt'] = self.convert_updated_at(updated_at) + ' ago'
+        return new_obj
+
+    def convert_updated_at(self, updated_at_ms):
+        updated_at_sec = float(updated_at_ms) / 1000.0
+        value = datetime.datetime.fromtimestamp(updated_at_sec)
+        delta = datetime.datetime.now() - value
+        return str(delta - datetime.timedelta(microseconds=delta.microseconds))
+
+    def update_column_width(self, obj, column_name, col_wid_dict):
+        w = col_wid_dict.get(column_name, 0)
+        w = max(w, len(str(obj.get(column_name, ''))))
+        col_wid_dict[column_name] = w
+
+    def parse_index_key(self, index_key):
+        """
+        parse index key and return table, column and suffix
+
+        currently recognized format is
+
+        table:column[.suffix]
+        """
+        first_colon = index_key.find(':')
+        if first_colon < 0:
+            return '', '', ''
+        table = index_key[0: first_colon]
+        last_dot = index_key.rfind('.')
+        if last_dot < 0:
+            column = index_key[first_colon + 1:]
+            suffix = ''
+        else:
+            column = index_key[first_colon + 1: last_dot]
+            suffix = index_key[last_dot + 1:]
+        return table, column, suffix
+
