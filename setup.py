2fb3c507e474f5f0a46c82c2459c739269ef80d3
Vadim Kurland <vadim@happygears.net>
2019-02-12 22:11:49 -0800

    v1.0.29

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='nsgcli',
-                 version='1.0.28',
+                 version='1.0.29',
                  description='NetSpyGlass CLI',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
